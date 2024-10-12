3321bbf447f032be7cf9c2b4f4f8b9d74eb0967a
anjalinarang <aknarang03@gmail.com>
2024-10-12 19:12:25 -0400

    summary works

--- NJCollegesAndMap/controllers/CollegesSummaryViewController.swift
+++ NJCollegesAndMap/controllers/CollegesSummaryViewController.swift
@@ -9,12 +9,28 @@ import UIKit
 
 class CollegesSummaryViewController: UIViewController {
     
+    let collegesModel = NJCollegesModel.sharedInstance
     
-    
+    @IBOutlet weak var totalText: UILabel!
+    @IBOutlet weak var northText: UILabel!
+    @IBOutlet weak var southText: UILabel!
+    @IBOutlet weak var centralText: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         testModel()
+        showSummary()
+    }
+    
+    func showSummary() {
+        
+        let data = collegesModel.getSummaryValues()
+        
+        totalText.text = String(data[0])
+        northText.text = String(data[1])
+        centralText.text = String(data[2])
+        southText.text = String(data[3])
+        
     }
 
 }
