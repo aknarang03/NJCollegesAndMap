d94df772b539392cb9c60cf30683c5bc4efb4d63
anjalinarang <aknarang03@gmail.com>
2024-10-12 13:55:59 -0400

    Initial Commit

---
+++ NJCollegesAndMapTests/NJCollegesAndMapTests.swift
@@ -0,0 +1,17 @@
+//
+//  NJCollegesAndMapTests.swift
+//  NJCollegesAndMapTests
+//
+//  Created by Anjali Narang  on 10/12/24.
+//
+
+import Testing
+@testable import NJCollegesAndMap
+
+struct NJCollegesAndMapTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
