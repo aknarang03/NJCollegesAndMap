03f93b4416611b2fb28bbbcb620fdfd95b9d77da
anjalinarang <aknarang03@gmail.com>
2024-10-12 16:18:52 -0400

    associate views with controllers

---
+++ NJCollegesAndMap/controllers/CollegesListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CollegesListTableViewCell.swift
+//  NJCollegesAndMap
+//
+//  Created by Anjali Narang  on 10/12/24.
+//
+
+import UIKit
+
+class CollegesListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
