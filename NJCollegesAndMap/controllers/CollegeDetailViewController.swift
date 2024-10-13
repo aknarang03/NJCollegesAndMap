7588154143cf26ff442f0742dc7414131670b918
anjalinarang <aknarang03@gmail.com>
2024-10-13 11:38:58 -0400

    marker in detail view map

--- NJCollegesAndMap/controllers/CollegeDetailViewController.swift
+++ NJCollegesAndMap/controllers/CollegeDetailViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import MapKit
 
-class CollegeDetailViewController: UIViewController {
+class CollegeDetailViewController: UIViewController, MKMapViewDelegate {
     
     var showCollegeId: Int?
     var selectedCollege: College?
@@ -22,8 +22,8 @@ class CollegeDetailViewController: UIViewController {
     @IBOutlet weak var phone: UILabel!
     @IBOutlet weak var map: MKMapView!
     
-    
     override func viewDidLoad() {
+        map.delegate = self
         super.viewDidLoad()
     }
     
@@ -41,7 +41,7 @@ class CollegeDetailViewController: UIViewController {
             if (selectedCollege != nil) {
                 
                 setViewpoint()
-                
+                addCollegeToMap()
                 
                 name.text = selectedCollege!.name
                 addressLine1.text = collegesModel.formatAddress(input: "\(selectedCollege!.address) \(selectedCollege!.address2)")
@@ -67,6 +67,40 @@ class CollegeDetailViewController: UIViewController {
         let coordinate = CLLocationCoordinate2D(latitude: selectedCollege!.coordinates[1], longitude: selectedCollege!.coordinates[0])
         let region = MKCoordinateRegion(center: coordinate, latitudinalMeters: 1000, longitudinalMeters: 1000)
         map.setRegion(region, animated: true)
+    }
+    
+    func addCollegeToMap() {
+        
+        let logoString = collegesModel.getImage(collegeName: selectedCollege!.name)
+            
+        let annotation = CollegeAnnotation(latitude: selectedCollege!.coordinates[1], longitude: selectedCollege!.coordinates[0], title: selectedCollege!.name, logoString: logoString, id: selectedCollege!.id)
+                
+        map.addAnnotation(annotation)
+        
+    }
+    
+    // annotation view
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+                
+        guard !(annotation is MKUserLocation) else {
+            return nil
+        }
+                
+        let collegeAnnotation = annotation as? CollegeAnnotation
+        
+        let identifier = "CollegeAnnotation"
+        let annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                    
+        annotationView.image = UIImage(named: collegeAnnotation?.logoString! ?? "default_uni")
+        annotationView.frame = CGRect(x: 0, y: 0, width: 25, height: 25)
+                
+        annotationView.canShowCallout = true
+        annotationView.calloutOffset = CGPoint(x: -5.0, y: 5.0)
+                
+        let imageView = UIImageView (frame: CGRect(origin: CGPoint.zero, size: CGSize(width: 60.0, height: 60.0)))
+        imageView.image = UIImage(named: collegeAnnotation?.logoString! ?? "default_uni")
+        
+        return annotationView
     }
 
 }
